530b59d6824a710a3258fd88eea150b0e865f9b1
MarcioDzn <marcio.roberto0739@gmail.com>
2026-08-31 20:01:22 -0300

    feat: add children to Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,31 @@
-import type { MouseEventHandler } from "react"
+import type { MouseEventHandler, ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
 type Props = {
-    text: string
-    type: "button" | "submit"
+    text?: string
+    children?: ReactNode
+    type?: "button" | "submit"
+    className?: string
     onClick: MouseEventHandler<HTMLButtonElement>
 }
 
-export default function Button({ text, type, onClick }: Props) {
+export default function Button({ 
+    text, 
+    children, 
+    type = "button", 
+    className, 
+    onClick 
+}: Props) {
     return (
         <button 
             type={type}
             onClick={onClick}
-            className="border border-transparent cursor-pointer bg-black py-2.5 px-4 font-medium text-white rounded-lg text-sm hover:bg-gray-700">
-                {text}
+            className={twMerge(
+                "border border-transparent cursor-pointer bg-black py-2.5 px-4 font-medium text-white rounded-lg text-sm hover:bg-gray-700",
+                className
+            )}
+        >
+                {children ?? text}
         </button>
     )
 }
